13a5f91d0cebfa0a8641bf8af184fbc57162fd4c
Imran Ahmed <114012876+imran0202@users.noreply.github.com>
2023-02-18 23:00:46 +0600

    Add files via upload

---
+++ changepassword.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Change Password</title>
+</head>
+<body>
+           
+        <fieldset >
+            <legend>CHANGE PASSWORD</legend>
+              
+            <table>
+                <tr>
+                    <td>Current Password:</td>
+                    <td><input type="password" name="" value="" placeholder=""/></td>
+                </tr>
+                <tr>
+                    <td>New Password:</td>
+                    <td><input type="password" name="" value="" placeholder=""/></td>
+                </tr>
+                <tr>
+                    <td>Retype New Password:</td>
+                    <td><input type="password" name="" value="" placeholder=""/></td>
+                </tr>
+               
+                
+                <tr align="left">
+                    <td>
+                        <input type="submit" name="" value="Submit"> 
+                    </td>
+                </tr>
+            </table>
+        </fieldset>
+    </fieldset>
+    
+    </form>
+</body>
+</html>
